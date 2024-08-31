d1b24a58771cdd5fe1a8ae79368a05088ec0d4da
Wasealex <wasealex27@gmail.com>
2024-08-31 10:44:55 +0300

    chore: Update Redis client to use new connection handling

--- utils/redis.js
+++ utils/redis.js
@@ -1,15 +1,15 @@
-import { createClient } from "redis";
-import { promisify } from "util";
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.isClientConnected = true;
-    this.client.on("error", (error) => {
+    this.client.on('error', (error) => {
       console.error(`Redis client not connected to the server: ${error}`);
       this.isClientConnected = false;
     });
-    this.client.on("connect", () => {
+    this.client.on('connect', () => {
       this.isClientConnected = true;
     });
   }
